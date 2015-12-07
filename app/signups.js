ca8362bd56e5f2f75806003b2e2c762d549663ad
Marcus Hellberg <marcus@vaadin.com>
2015-12-07 14:45:34 -0800

    array datasource

---
+++ app/signups.js
@@ -0,0 +1,91 @@
+var signups = [{
+  user: {
+    name: {
+      first: 'josephine',
+      last: 'bradley'
+    },
+    email: 'josephine.bradley@example.com',
+    phone: '(603)-836-7013'
+  }
+}, {
+  user: {
+    name: {
+      first: 'lois',
+      last: 'dixon'
+    },
+    email: 'lois.dixon@example.com',
+    phone: '(011)-530-6547'
+  }
+}, {
+  user: {
+    name: {
+      first: 'andy',
+      last: 'crawford'
+    },
+    email: 'andy.crawford@example.com',
+    phone: '(067)-431-8686'
+  }
+}, {
+  user: {
+    name: {
+      first: 'erik',
+      last: 'ortiz'
+    },
+    email: 'erik.ortiz@example.com',
+    phone: '(180)-687-7569'
+  }
+}, {
+  user: {
+    name: {
+      first: 'brooklyn',
+      last: 'brown'
+    },
+    email: 'brooklyn.brown@example.com',
+    phone: '(490)-068-3349'
+  }
+}, {
+  user: {
+    name: {
+      first: 'clifton',
+      last: 'mcdonalid'
+    },
+    email: 'clifton.mcdonalid@example.com',
+    phone: '(385)-989-2806'
+  }
+}, {
+  user: {
+    name: {
+      first: 'mabel',
+      last: 'mitchell'
+    },
+    email: 'mabel.mitchell@example.com',
+    phone: '(554)-627-7735'
+  }
+}, {
+  user: {
+    name: {
+      first: 'carrie',
+      last: 'wheeler'
+    },
+    email: 'carrie.wheeler@example.com',
+    phone: '(924)-817-4481'
+  }
+}, {
+  user: {
+    name: {
+      first: 'harry',
+      last: 'jensen'
+    },
+    email: 'harry.jensen@example.com',
+    phone: '(890)-459-9412'
+  }
+}, {
+  user: {
+    name: {
+      first: 'beth',
+      last: 'hall'
+    },
+    email: 'beth.hall@example.com',
+    phone: '(817)-639-9011'
+  }
+}];
